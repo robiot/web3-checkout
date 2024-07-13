a540d51aae84efab4215c0bef2da84f5bfa15301
Elliot <elliot@robiot.dev>
2024-07-14 01:24:43 +0300

    Load stuff on checkout page

--- ui/frontend/src/app/(checkout)/checkout/[slug]/page.tsx
+++ ui/frontend/src/app/(checkout)/checkout/[slug]/page.tsx
@@ -1,58 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import Image from "next/image";
-import { useAccount } from "wagmi";
 
 import { Container } from "@/components/common/Container";
 import { Stars } from "@/components/common/Stars";
+import { Spinner } from "@/components/ui/spinner";
+import { useProductsSingle } from "@/lib/useProductsSingle";
 
 import { ConnectPage } from "./_components/ConnectPage";
 import { PayPage } from "./_components/PayPage";
 import { Swap } from "./_components/Swap";
 import { VerifyPage } from "./_components/VerifyPage";
 
-const CheckoutPage = () => {
-  // const data = useQuery({
-  //   queryKey: ["products"],
-  //   queryFn: () => {
-  //     return [
-  //       {
-  //         id: "192h9d28du",
-  //         name: "Product 1",
-  //         price: 100,
-  //         totalSales: 100,
-  //       },
-  //     ];
-  //   },
-  // });
-  const account = useAccount();
+interface PageProperties {
+  params: {
+    slug: string;
+  };
+}
+
+const formatCurrency = (number_?: number) => ((number_ ?? 0) / 100).toFixed(2);
+
+function CheckoutPage({ params }: PageProperties) {
+  const product = useProductsSingle(params.slug);
+
+  if (product.isLoading)
+    return (
+      <div className="flex items-center justify-center w-full h-96">
+        <Spinner size="sm" className="text-background" />
+      </div>
+    );
 
   return (
     <div className="flex flex-col md:flex-row h-screen mx-auto">
       <div className="h-screen md:w-1/2 bg-[#F7F7F8] p-5">
         <Container className="ml-auto mr-20 max-w-[30rem] flex flex-col">
-          <div className="">TheCompany</div>
+          <div className="">{product.data?.created_by}</div>
 
-          <div className="mt-14 text-2xl">Coolest Afterparty Ticket</div>
+          <div className="mt-14 text-2xl">{product.data?.name}</div>
 
           <div className="flex gap-3 items-center mt-6">
-            <span className="text-3xl">$9.00</span>
-            <span className="text-sm text-foreground/70">+$2.0 GAS</span>
+            <span className="text-3xl">
+              ${formatCurrency(product.data?.price)}
+            </span>
+            {/* <span className="text-sm text-foreground/70">+$2.0 GAS</span> */}
           </div>
 
-          <Image
-            src="/tmp.jpg"
+          <img
+            src={product.data?.media.at(0)}
             alt="Asda"
             width={651}
             height={390}
             className="rounded-xl mt-6 h-72 object-cover"
           />
 
-          <p className="text-foreground/60 mt-8">
-            Here you can talk more about the product in whatever format you
-            would like, most likely in markdown sin ce that is what is
-            formatted.
-          </p>
+          <p className="text-foreground/60 mt-8">{product.data?.description}</p>
 
           <p className="flex items-center gap-2 mt-auto">
             Powered by
@@ -86,7 +88,7 @@ const CheckoutPage = () => {
 
           <div className="flex justify-between py-6">
             <span>Total</span>
-            <span>9 USD</span>
+            <span>{formatCurrency(product.data?.price)} USD</span>
           </div>
 
           <div className="border-t border-border w-full" />
@@ -104,6 +106,6 @@ const CheckoutPage = () => {
       </div>
     </div>
   );
-};
+}
 
 export default CheckoutPage;
